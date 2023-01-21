1be958a4aa1a4ba31027490eed9f1cfb6dcb7be6
rjbrown3 <70820550+rjbrown3@users.noreply.github.com>
2023-01-21 16:25:34 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace QuoteFun_rjb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteTools qt = new QuoteTools();
+
+            string userQuote = "";
+
+            Console.WriteLine("Welcome to the Quote Analyzer!");
+            Console.WriteLine("Please enter a quote");
+
+            userQuote = Console.ReadLine();
+
+            Console.WriteLine("Characters: " + qt.CharacterCount(userQuote));
+            Console.WriteLine("# Words: " + qt.WordCount(userQuote));
+            Console.WriteLine("Count of letters:");
+            qt.LetterCount(userQuote);
+        }
+    }
+}
---
+++ QuoteTools.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuoteFun_rjb
+{
+    public class QuoteTools
+    {
+        //count # words 
+
+        public int WordCount(string quote)
+        {
+            string[] words = quote.Split(' ');
+            int numWords = 0;
+
+            numWords = words.Length;
+
+            return numWords;
+        }
+
+        //Count # characters 
+        public int CharacterCount(string quote)
+        {
+            int numCharacters = 0;
+            numCharacters = quote.Length;
+
+            return numCharacters;
+        }
+
+        //Letter count 
+        public void LetterCount(string quote)
+        {
+            char ch = ' ';
+            int[] letters = new int[26];
+
+            //count the letters
+            for (int i = 0; i < quote.Length; i++)
+            {
+                ch = quote[i];
+                ch = Char.ToUpper(ch);
+
+                //check to see if the character is a letter
+                if (ch >= 'A' && ch <= 'Z')
+                {
+                    letters[(ch - 65)]++;
+                }
+            }
+            
+            //print results
+            for (int i = 0; i < letters.Length; i++)
+            {
+                char letterSymbol = ' ';
+
+                letterSymbol = (char) (i + 65);
+
+                Console.WriteLine(letterSymbol + ": " + letters[i]);
+            }
+        }
+    }
+}
